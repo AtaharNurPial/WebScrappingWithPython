8451b217158d3406ea489a02589d3ba3f4a11e41
Atahar Nur Pial <atp82232@gmail.com>
2021-11-22 10:44:41 +0600

    crawling engine added

--- main.py
+++ main.py
@@ -7,27 +7,42 @@ url = 'https://www.timesjobs.com/candidate/job-search.html?searchType=personaliz
 
 timestamp = date.today()
 
-main_sauce = requests.get(url).text
-sauce = BeautifulSoup(main_sauce, 'lxml')
+def getData(url):
+    main_sauce = requests.get(url).text
+    sauce = BeautifulSoup(main_sauce, 'lxml')
+    all_job = sauce.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
 
-all_job = sauce.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+    for index, job in enumerate(all_job):
+        job_count = sauce.find('header', class_ = 'srp-header clearfix')
+        job_found = job_count.h1.span.text
+        comp_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ','')
+        exp = job.ul.li.text[11:]
+        jd = job.find('ul', class_ = 'list-job-dtl clearfix')
+        job_desc = jd.li.text
+        skills = job.find('span', class_ = 'srp-skills').text.replace(' ','')
+        posted_date = job.find('span', class_ = 'sim-posted').span.text
+        more_info = job.header.h2.a['href']
+        with open(f'Info\InfofileNo{index}_{timestamp}.txt', 'w') as info_file:
+            info_file.write(f"Total Job : {job_found} \n")
+            info_file.write(f"Company Name : {comp_name.strip()}\n")
+            info_file.write(f"Required Experience : {exp}\n")
+            info_file.write(f"{job_desc.strip()}\n")
+            info_file.write(f"Required Skills : {skills.strip()}\n")
+            info_file.write(f"Posted Date : {posted_date.strip()}\n")
+            info_file.write(f"More Information : {more_info}\n")
 
-for index, job in enumerate(all_job):
-    job_count = sauce.find('header', class_ = 'srp-header clearfix')
-    job_found = job_count.h1.span.text
-    comp_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ','')
-    exp = job.ul.li.text[11:]
-    jd = job.find('ul', class_ = 'list-job-dtl clearfix')
-    job_desc = jd.li.text
-    skills = job.find('span', class_ = 'srp-skills').text.replace(' ','')
-    posted_date = job.find('span', class_ = 'sim-posted').span.text
-    more_info = job.header.h2.a['href']
-    with open(f'Info\InfofileNo{index}_{timestamp}.txt', 'w') as info_file:
-        info_file.write(f"Total Job : {job_found} \n")
-        info_file.write(f"Company Name : {comp_name.strip()}\n")
-        info_file.write(f"Required Experience : {exp}\n")
-        info_file.write(f"{job_desc.strip()}\n")
-        info_file.write(f"Required Skills : {skills.strip()}\n")
-        info_file.write(f"Posted Date : {posted_date.strip()}\n")
-        info_file.write(f"More Information : {more_info}\n")
+def pageCrawler(sauce):
+    pages = sauce.find('div', class_ = 'srp-pagination clearfix')
+    if not pages.find('em', class_ = 'nxtC'):
+        url = 'https://www.timesjobs.com' + str(pages.find('div', class_ = 'srp-pagination clearfix').find_all('em').a['href'])
+        return url
+    else:
+        return
+
+while True:
+    sauce = getData(url)
+    url = pageCrawler(sauce)
+    if not url:
+        break
+    getData(url)
 
